e978fbe50990fc0d2ee8b98e920d9d632796fccc
Jose Alberto Leyva Contreras <josealc1998@gmail.com>
2024-07-02 16:44:51 -0700

    refactor render index file

--- app/renderer/index.js
+++ app/renderer/index.js
@@ -4,18 +4,18 @@ const { ipcRenderer } = require('electron');
 // DOM elements
 const products = document.querySelector('#products');
 
-// Event listeners
-ipcRenderer.on('new-product', (e, newProduct) => {
-    const newProductTemplate = `
+// Functions
+function createProductTemplate(product) {
+    return `
         <div class="col-sm-4 p-2">
             <div class="card text-center">
                <div class="card-header">
-                    <h5 class="card-title">${newProduct.name}</h5>
+                    <h5 class="card-title">${product.name}</h5>
                </div>
                <div class="card-body">
-                    <p class="card-text">Description: ${newProduct.description}</p>
+                    <p class="card-text">Description: ${product.description}</p>
                     <hr>
-                    <p class="card-text">Price: $${newProduct.price}</p>
+                    <p class="card-text">Price: $${product.price}</p>
                </div>
                <div class="card-footer">
                     <button class="btn btn-danger btn-sm">Delete</button>
@@ -23,16 +23,24 @@ ipcRenderer.on('new-product', (e, newProduct) => {
             </div>
         </div>
     `;
-    products.innerHTML += newProductTemplate;
-    const btns = document.querySelectorAll('.btn.btn-danger');
+}
 
-    btns.forEach(btn => {
-        btn.addEventListener('click', (e) => {
-            e.target.parentElement.parentElement.parentElement.remove();
-        });
-    });
+function addProduct(product) {
+    const newProductTemplate = createProductTemplate(product);
+    products.innerHTML += newProductTemplate;
+}
+
+// Event listeners
+ipcRenderer.on('new-product', (e, newProduct) => {
+    addProduct(newProduct);
 });
 
 ipcRenderer.on('remove-all-products', () => {
     products.innerHTML = '';
+});
+
+products.addEventListener('click', (e) => {
+    if (e.target.classList.contains('btn-danger')) {
+        e.target.parentElement.parentElement.parentElement.remove();
+    }
 });
